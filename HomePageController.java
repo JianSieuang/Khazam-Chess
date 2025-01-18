5b3a9d9367394e4c59db4a72416214a0166341c2
Cresswell <cresswelllew@gmail.com>
2025-01-18 13:06:26 +0800

    update home page btn action

--- HomePageController.java
+++ HomePageController.java
@@ -1,5 +1,9 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
 public class HomePageController 
 {
@@ -16,6 +20,7 @@ public class HomePageController
 
     private void initializeListeners() 
     {
+        buttonSound(view.getHomePanel().getNewGameButton());
         view.getHomePanel().getNewGameButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) 
@@ -25,6 +30,7 @@ public class HomePageController
             }
         });
 
+        buttonSound(view.getHomePanel().getLoadGameButton());
         view.getHomePanel().getLoadGameButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) 
@@ -34,6 +40,7 @@ public class HomePageController
             }
         });
 
+        buttonSound(view.getHomePanel().getSettingsButton());
         view.getHomePanel().getSettingsButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) 
@@ -43,6 +50,7 @@ public class HomePageController
             }
         });
 
+        buttonSound(view.getHomePanel().getQuitGameButton());
         view.getHomePanel().getQuitGameButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) 
@@ -52,6 +60,25 @@ public class HomePageController
                 System.exit(0);
             }
         });
+    }
+
+    private void buttonSound(JButton button) {
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setBackground(button.getBackground().darker());
+                button.setForeground(Color.WHITE);
+                new BtnSound("hover").actionPerformed(null);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setBackground(button.getBackground().brighter());
+                button.setForeground(Color.BLACK);
+            }
+        });
+
+        button.addActionListener(e -> new BtnSound("click").actionPerformed(null));
     }
 
     public static void main(String[] args) 
