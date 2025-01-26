ada6ada59317330ae247524faaa1ef9b97936d6e
Chong <jinxuanzhang1@gmail.com>
2025-01-26 17:42:47 +0800

    add comment

--- GameBoard.java
+++ GameBoard.java
@@ -44,15 +44,18 @@ public class GameBoard {
     }
 
     public void newGame() {
-        // erase the board data
+        // erase the old board data
         for (GamePiece[] row : board) {
             Arrays.fill(row, null);
         }
+        // call this function to reset the board and game status
         initBoard();
+        // load the board color
         loadBoardColor();
     }
 
     private void initBoard() {
+        // these are initialize data
         move = "Blue";
         turn = 0;
         board[0][0] = new TorPiece(0, 0, "Red");
@@ -66,7 +69,8 @@ public class GameBoard {
         board[7][2] = new SauPiece(7, 2, "Blue");
         board[7][3] = new BizPiece(7, 3, "Blue");
         board[7][4] = new TorPiece(7, 4, "Blue");
-
+        
+        // using for loop to create rma piece
         for (int i = 0; i < col; i++) {
             board[1][i] = new RamPiece(1, i, "Red");
             board[6][i] = new RamPiece(6, i, "Blue");
@@ -74,20 +78,36 @@ public class GameBoard {
     }
 
     public void saveGame() {
+        // save the game
         try (PrintWriter writer = new PrintWriter("game.txt")) {
+            // save turn and move
             writer.println("Turn: " + turn);
             writer.println("Move: " + move);
+            // enter a space
             writer.println();
-
+            
+            // show how to read the board in txt file
+            writer.println("Explaination: ");
+            writer.println("The '.' represents an empty space on the board, indicating no piece is present in that position");
+            writer.println("Each name consists of two parts: the type of piece and the team color");
+            writer.println("The '_Flip' indicating a flipped state of the piece");
+            // enter a space
+            writer.println();
+            
+            // the board :)
             writer.println("Board:");
+            // save all the board details
             for (GamePiece[] row : board) {
                 for (GamePiece piece : row) {
                     if (piece != null) {
+                        // if have piece, get the name and save it, will save like "Ram_Red"
                         writer.print(piece.getPieceName() + " ");
                     } else {
+                        // if does not have piece will save a dot "." to represent null
                         writer.print(". ");
                     }
                 }
+                // this is for next line based on the board
                 writer.println();
             }
         } catch (IOException e) {
@@ -96,30 +116,41 @@ public class GameBoard {
     }
 
     public void loadGame() {
+        // load the game
         try (Scanner scanner = new Scanner(new File("game.txt"))) {
+            // scan the first line
             String turnLine = scanner.nextLine();
+            // turnLine will get "Turn: 1" for exmaple
+            // using split(": ") to split Turn and 1 then [1] to take second value
+            // Integer.parseInt is change the datatype to int for the second value 
             turn = Integer.parseInt(turnLine.split(": ")[1]);
-
+            
+            // scan the second line
             String moveLine = scanner.nextLine();
+            // turnLine will get "Move: Blue" for exmaple
+            // using split(": ") to split Move and Blue then [1] to take second value
             this.move = moveLine.split(": ")[1];
 
-            if (scanner.hasNextLine()) {
-                scanner.nextLine();
+            // skip the Explaination part 
+            while (scanner.hasNextLine()) {
+                // the scanner will read until board: exist
+                if(scanner.nextLine().startsWith("Board:")) break;
             }
 
-            if (scanner.hasNextLine()) {
-                scanner.nextLine();
-            }
-
+            // the content of the board
             for (int i = 0; i < row; i++) {
+                // split but each piece including the dot
                 String[] line = scanner.nextLine().split(" ");
                 for (int j = 0; j < col; j++) {
                     String piece = line[j];
                     if (!piece.equals(".")) {
+                        // if is not a dot, means is a piece
                         String[] parts = piece.split("_", 2);
+                        // split into two parts: piece type and the player
                         String type = parts[0];
                         String player = parts[1];
 
+                        // check the type
                         switch (type) {
                             case "Tor":
                                 board[i][j] = new TorPiece(i, j, player);
@@ -140,6 +171,7 @@ public class GameBoard {
                                 throw new IllegalArgumentException("Unknown piece type: " + type);
                         }
                     } else {
+                        // if is a dot, declare the position as null
                         board[i][j] = null;
                     }
                 }
@@ -147,16 +179,22 @@ public class GameBoard {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+        // load the board color
         loadBoardColor();
     }
 
     private void loadBoardColor() {
         try (BufferedReader reader = new BufferedReader(new FileReader("setting.txt"))) {
             String line;
+            // reader read the line one by one until is null
             while ((line = reader.readLine()) != null) {
                 if (line.startsWith("Primary Color")) {
+                    // if the string starts with primary color, then take the value using split
                     primaryColor = line.split(" : ")[1];
+                      
                 } else if (line.startsWith("Secondary Color")) {
+                    // if the string starts with secondary color, then take the value using split
                     secondaryColor = line.split(" : ")[1];
                 }
             }
@@ -165,16 +203,26 @@ public class GameBoard {
         }
     }
 
+    // get the primary color, main reason is for view can get the color
     public String getPrimaryColor() {
+        // if no primary color then give a default color
         return primaryColor != null ? primaryColor : "#FFFFFF";
     }
 
+    // get the secondary color, main reason is for view can get the color
     public String getSecondaryColor() {
+        // if no secondary color then give a default color
         return secondaryColor != null ? secondaryColor : "#000000";
     }
 
+    // turn the board, flip the board
     private void turnBoard() {
         if (winner == null) {
+            // flip the board is like change the piece left to right and up to down
+            // while simplify it become swap the piece at 0,0 to 7,4 , 1,0 to 6,4
+            // in that case can write like i swap with row - 1 - i, ii swap with col - 1 - ii
+            // and fliping it just half times of the total of the piece
+            // can use row/2 or col/2 in the loop
             for (int i = 0; i < row / 2; i++) {
                 for (int ii = 0; ii < col; ii++) {
                     GamePiece temp = board[i][ii];
@@ -182,19 +230,23 @@ public class GameBoard {
                     board[row - 1 - i][col - 1 - ii] = temp;
                 }
             }
+            // update the position
             for (int i = 0; i < row; i++) {
                 for (int ii = 0; ii < col; ii++) {
                     if (board[i][ii] != null) {
+                        // let every piece update their position every time the board turn around
                         board[i][ii].updatePosition(i, ii);
                     }
                 }
             }
         }
 
+        // after turn the board, clear the selected piece
         clearSelectedPieceAndSteps();
     }
 
     private void clearSelectedPieceAndSteps() {
+        // clear the steps and position of the seleceted piece
         moveableSteps = new int[0][0];
         capturableSteps = new int[0][0];
         selectedPiece = null;
@@ -203,13 +255,19 @@ public class GameBoard {
     }
 
     private void countMove() {
+        // change the move to the next player
         move = move.equals("Blue") ? "Red" : "Blue";
+        // if the blue player plays agian, the turn + 1, consider as one round
         turn = move.equals("Blue") ? turn + 1 : turn;
 
+        // while count the move, will do the checking for the tor and xor piece 
+        // every 2 turns, will change the form
         if (turn % 2 == 0 && move.equals("Blue")) {
             for (int i = 0; i < row; i++) {
                 for (int ii = 0; ii < col; ii++) {
                     if (board[i][ii] != null) {
+                        // if is TorPiece, change to XorPiece while is XorPiece will change to TorPiece
+                        // just to replace it only :)
                         if (board[i][ii] instanceof TorPiece) {
                             board[i][ii] = new XorPiece(i, ii, board[i][ii].getPlayer());
                         } else if (board[i][ii] instanceof XorPiece) {
@@ -224,14 +282,18 @@ public class GameBoard {
     public void selectPiece(int r, int c) {
         if (winner != null)
             return;
+        // check the r and c is in the board
         if (r < row && r >= 0 && c < col && c >= 0) {
+            // if user click the same piece, consider as unselect the piece
             if (selectedPiece != null && selectedRow == r && selectedCol == c) {
                 clearSelectedPieceAndSteps();
                 return;
             } else if (board[r][c] != null && board[r][c].player.equals(move)) {
+                // store the info of the selected piece
                 selectedPiece = board[r][c];
                 selectedRow = r;
                 selectedCol = c;
+                // get the moveable and capturable steps
                 moveableSteps = selectedPiece.moveable(board);
                 capturableSteps = selectedPiece.capturable(board);
             }
@@ -240,24 +302,35 @@ public class GameBoard {
     }
 
     public boolean putPiece(int r, int c) {
+        // without selected piece, return false
         if (selectedPiece == null)
             return false;
+            
+         // check the r and c is in the board 
         if (r < row && r >= 0 && c < col && c >= 0) {
+            // if the user click other pieces
             if (board[r][c] != null) {
+                // check is different player piece or not
                 if (board[r][c].getPlayer() != selectedPiece.getPlayer()) {
                     for (int i = 0; i < capturableSteps.length; i++) {
+                        // check all capturable step to determine is valid or not
                         if (capturableSteps[i][0] == r && capturableSteps[i][1] == c) {
+                            // if the captured piece is SauPiece
                             if (board[r][c] instanceof SauPiece) {
+                                // save the winner
                                 winner = selectedPiece.getPlayer();
-
+                                
+                                // delete the file because the game is end
                                 File file = new File("game.txt");
-
                                 if (file.exists()) {
                                     file.delete();
                                 }
                             }
+                            //replace the piece with selected piece
                             board[r][c] = selectedPiece;
+                            // remove the selected piece from original place
                             board[selectedRow][selectedCol] = null;
+                            // count the move and turn the board
                             countMove();
                             turnBoard();
                             return true;
@@ -265,11 +338,16 @@ public class GameBoard {
                     }
                 }
             } else {
+                // if user click empty space
                 if (r != selectedRow || c != selectedCol) {
                     for (int i = 0; i < moveableSteps.length; i++) {
+                        // check all moveavle step to determine is valid or not
                         if (moveableSteps[i][0] == r && moveableSteps[i][1] == c) {
+                            //replace the piece with selected piece
                             board[r][c] = selectedPiece;
+                            // remove the selected piece from original place
                             board[selectedRow][selectedCol] = null;
+                            // count the move and turn the board
                             countMove();
                             turnBoard();
                             return true;
@@ -279,10 +357,12 @@ public class GameBoard {
             }
         }
 
+        //put the piece back to board
         board[selectedRow][selectedCol] = selectedPiece;
         return false;
     }
 
+    // all encapsulation below
     public String getWinner() {
         return winner;
     }
--- HomePageController.java
+++ HomePageController.java
@@ -118,13 +118,13 @@ public class HomePageController {
             }
 
             @Override
-            public void mouseExited(MouseEvent e) { // mouse exit from the button
+            public void mouseExited(MouseEvent e) { 
+                // mouse exit from the button
                 button.setBackground(button.getBackground().brighter()); // button background be brighter when exit
                 button.setForeground(Color.BLACK); // text be back color back when the mouse exit the button
             }
         });
 
-        button.addActionListener(e -> new BtnSound("click", settingController).actionPerformed(null)); // play click
-                                                                                                       // sound
+        button.addActionListener(e -> new BtnSound("click", settingController).actionPerformed(null)); // play click sound
     }
 }
